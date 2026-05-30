f25c34e5fbe5af845944205f9489db34acbe4068
sajad <ssmzr0@gmail.com>
2026-05-30 19:11:07 +0430

    tekrar

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
+#8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    MessageHandler,
+    ContextTypes,
+    filters
+)
 
 TOKEN = "8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! من اولین ربات تو هستم 😎")
+async def tekrar (update : Update ,context : ContextTypes.DEFAULT_TYPE ) :
+    text = update.message.text
+    await update.message.reply_text(f"tekrar: {text}")
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+app.add_handler(
+    MessageHandler(filters.TEXT ,tekrar)
+)
 
 print("ربات روشن شد...")
 
