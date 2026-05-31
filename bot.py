123be1dabfe5c30b70ce560140947f7654ad3138
sajad <ssmzr0@gmail.com>
2026-05-31 12:50:27 +0430

    v4

--- bot.py
+++ bot.py
@@ -1,74 +1,67 @@
 #8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34
 from telegram import Update
-from telegram import KeyboardButton
-from telegram import ReplyKeyboardMarkup
+
 
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
     filters,
     MessageHandler,
-    CommandHandler
+    CommandHandler,
+    ConversationHandler
 )
 
 TOKEN = "8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34"
+NAME, AGE = range(2)
+
+async def start (update: Update, context: ContextTypes.DEFAULT_TYPE) :
+    await update.message.reply_text("اسمت چیه ؟")
+    return NAME
+
+async def get_name (update: Update, context: ContextTypes.DEFAULT_TYPE) :
+    context.user_data["name"] = update.message.text
+    await update.message.reply_text("چند سالته ؟")
+    return AGE
+
+async def get_age (update: Update, context: ContextTypes.DEFAULT_TYPE) :
+    context.user_data["age"] = update.message.text
+    await update.message.reply_text(f'salam {context.user_data["name"]} to {context.user_data["age"]} salet hast.')
+    return ConversationHandler.END
 
 
-keyboard = [
-     [
-          KeyboardButton("راهنما"),
-          KeyboardButton("درباره ما")
-     ]
-]
-
-markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
-
-async def start (update: Update ,context: ContextTypes.DEFAULT_TYPE ) :
-    await update.message.reply_text(
-         "سلام به ربات من خوش آمدی",
-         reply_markup = markup
-                                    )
-
-async def help_command (update: Update ,context: ContextTypes.DEFAULT_TYPE) :
-    await update.message.reply_text("""
-    دستورات:
-    /start
-    /help
-    """)
-
-async def menu_handler (update: Update ,context: ContextTypes.DEFAULT_TYPE) :
-       text = update.message.text
-       if text == "راهنما" :
-            await update.message.reply_text("""
-                دستورات:
-                /start
-                /help
-            """)
-
-       elif text == "درباره ما" :
-            await update.message.reply_text("این ربات برای آموزش ساخته شده است.")
-
-       else :
-            await update.message.reply_text(f"توگفتی: {text}")
-        
 
 
 
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(
-    CommandHandler("start", start)
+
+conv_handler = ConversationHandler(
+    entry_points=[
+        CommandHandler("start", start)
+    ],
+
+    states={
+        NAME: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                get_name
+            )
+        ],
+
+        AGE: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                get_age
+            )
+        ]
+    },
+
+    fallbacks=[]
 )
 
-app.add_handler(
-    CommandHandler("help",help_command )
-)
+app.add_handler(conv_handler)
 
-app.add_handler(
-     MessageHandler(filters.TEXT & ~filters.COMMAND, menu_handler)
-)
 print("ربات روشن شد...")
 
 app.run_polling()
