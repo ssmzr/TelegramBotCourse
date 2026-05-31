ea3e3b79f57de8ed3e3953302612591bf3123672
sajad <ssmzr0@gmail.com>
2026-05-31 11:13:46 +0430

    Message Handler And Command Handler

--- bot.py
+++ bot.py
@@ -3,20 +3,26 @@ from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
+    filters,
+    MessageHandler,
     CommandHandler
 )
 
 TOKEN = "8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34"
 
-async def start (update : Update ,context : ContextTypes.DEFAULT_TYPE ) :
+async def start (update: Update ,context: ContextTypes.DEFAULT_TYPE ) :
     await update.message.reply_text("سلام به ربات من خوش آمدی")
 
-async def help (update : Update ,context : ContextTypes.DEFAULT_TYPE) :
+async def help_command (update: Update ,context: ContextTypes.DEFAULT_TYPE) :
     await update.message.reply_text("""
     دستورات:
     /start
     /help
     """)
+
+async def echo (update: Update ,context: ContextTypes.DEFAULT_TYPE) :
+       text = update.message.text
+       await update.message.reply_text(f"تو گفتی : {text}")
 
 
 
@@ -27,7 +33,11 @@ app.add_handler(
 )
 
 app.add_handler(
-    CommandHandler("help",help )
+    CommandHandler("help",help_command )
+)
+
+app.add_handler(
+     MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
 )
 print("ربات روشن شد...")
 
