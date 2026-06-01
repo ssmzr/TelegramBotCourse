53fb39e9eb336943b5da0e3d946b345c8db428e5
sajad <ssmzr0@gmail.com>
2026-06-01 11:04:11 +0430

    v5

--- bot.py
+++ bot.py
@@ -1,67 +1,85 @@
 #8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34
 from telegram import Update
 
-
 from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
     filters,
+    ContextTypes,
+    ConversationHandler,
     MessageHandler,
     CommandHandler,
-    ConversationHandler
-)
+    ApplicationBuilder
+     )
 
 TOKEN = "8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34"
-NAME, AGE = range(2)
 
-async def start (update: Update, context: ContextTypes.DEFAULT_TYPE) :
+NAME, AGE, CITY = range(3)
+
+
+async def start (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("اسمت چیه ؟")
+
     return NAME
 
-async def get_name (update: Update, context: ContextTypes.DEFAULT_TYPE) :
+async def get_name (update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data["name"] = update.message.text
-    await update.message.reply_text("چند سالته ؟")
+    await update.message.reply_text("چند سالته؟ ")
+
     return AGE
 
-async def get_age (update: Update, context: ContextTypes.DEFAULT_TYPE) :
+async def get_age (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
     context.user_data["age"] = update.message.text
-    await update.message.reply_text(f'salam {context.user_data["name"]} to {context.user_data["age"]} salet hast.')
+    await update.message.reply_text("شهرت چیه؟ ")
+
+    return CITY
+
+async def get_city (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["city"] = update.message.text
+    await update.message.reply_text(
+    f"""
+✅ ثبت نام با موفقیت انجام شد
+
+👤 نام: {context.user_data["name"]}
+🎂 سن: {context.user_data["age"]}
+🏙 شهر: {context.user_data["city"]}
+"""
+)
+
     return ConversationHandler.END
 
-
-
-
-
-
 app = ApplicationBuilder().token(TOKEN).build()
-
 
 conv_handler = ConversationHandler(
     entry_points=[
         CommandHandler("start", start)
     ],
 
-    states={
-        NAME: [
-            MessageHandler(
-                filters.TEXT & ~filters.COMMAND,
-                get_name
-            )
+    states = {
+        NAME:[
+            MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)
         ],
 
         AGE: [
-            MessageHandler(
-                filters.TEXT & ~filters.COMMAND,
-                get_age
-            )
+            MessageHandler(filters.Text & ~filters.COMMAND, get_age)
+
+        ],
+        
+        CITY: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, get_city)
+
         ]
+
     },
 
-    fallbacks=[]
+    fallbacks= []
 )
 
 app.add_handler(conv_handler)
-
 print("ربات روشن شد...")
-
 app.run_polling()
+
+
+
+
+
+
