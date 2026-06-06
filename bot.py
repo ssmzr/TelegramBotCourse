70e357c988e9e793b45ba2aca87a8d463e399bbe
sajad <ssmzr0@gmail.com>
2026-06-06 18:44:31 +0430

    18

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from telegram.ext import (
 )
 
 TOKEN = "8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34"
+ADD_TASK , DELETE_TASK = range(2)
 
 keyboard = [
     [KeyboardButton("➕ افزودن کار")],
@@ -23,73 +24,187 @@ keyboard = [
 
 markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        context.user_data["tasks"] = []
+        if "tasks" not in context.user_data:
+              context.user_data["tasks"] = []
+
         await update.message.reply_text(" ",reply_markup=markup)
 
 
 
-async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        text = update.message.text
-        match(text):
-               case("➕ افزودن کار"):
-                      return add_task
-               case("📋 نمایش کارها"):
-                      return print_tasks
-               case("🗑 حذف همه کارها"):
-                      return dell_all
-               case("❌ حذف کار مد نظر"):
-                      return dell_by_index
-                      
-
-         
 
 
-async def add_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply("کار مورد نظر را وارد کن:")
-    task = update.message.text
-    context.user_data["tasks"] += task
-    await update.message.reply("به لیست کار ها اضافه شد ✅")
+
+async def start_add_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
+       await update.message.reply_text(
+              "کار مورد نظر را وارد کن:"
+       )
+       return ADD_TASK
+
+async def save_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
+       task = update.message.text
+       context.user_data["tasks"].append(task)
+       await update.message.reply_text(
+        "کار اضافه شد ✅"
+               )
+
+       return ConversationHandler.END
 
 
-async def print_tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
-       if context.user_data["tasks"] != [] :
-            await update.message.reply_text("لیست کار ها:")
-            for index, task in enumerate(context.user_data["tasks"], start=1) :
-                    await update.message.reply_text(f"{index}-->{task}")
-       else :
-             await update.message.reply_text("لیست کارها خالی است.") 
+
+async def display(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if not context.user_data.get("tasks"):
+        await update.message.reply_text("هیچ کاری ثبت نشده است.")
+        return ConversationHandler.END
+
+    else:
+        task_text = "\n".join(
+            f"{index}. {task}"
+            for index, task in enumerate(
+                context.user_data["tasks"],
+                start=1
+            )
+        )
+
+        await update.message.reply_text(task_text)
+        return ConversationHandler.END
 
 
-async def dell_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
-       context.user_data["tasks"] = []
-       await update.message.reply_text("همه کارها حذف شدند.")
+
+async def delight_all_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
+      if context.user_data["tasks"] != [] :
+            context.user_data["tasks"] = []
+            await update.message.reply_text( "همه کارها حذف شدند")
+
+      else:
+            await update.message.reply_text( "هیچ کاری برای حذف وجود ندارد")
 
 
-async def dell_by_index(update: Update, context: ContextTypes.DEFAULT_TYPE):
-       await update.message.reply_text("شماره کاری که میخواهید حذف بشه را وارد کنید :")
-       num = update.message.text
-       list_task = context.user_data["tasks"]
-       list_task.pop(int(num)-1)
-       await update.message.reply_text("کار حذف شد.")
 
+async def get_index(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-        
+    if not context.user_data["tasks"]:
+        await update.message.reply_text(
+            "هیچ کاری برای حذف وجود ندارد"
+        )
+        return ConversationHandler.END
+
+    task_text = "\n".join(
+        f"{index}. {task}"
+        for index, task in enumerate(
+            context.user_data["tasks"],
+            start=1
+        )
+    )
+
+    await update.message.reply_text(
+        f"""لیست کارها:
+
+{task_text}
+
+شماره کاری که می‌خواهی حذف شود را وارد کن:
+"""
+    )
+
+    return DELETE_TASK
+
+async def delete_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
+      if not update.message.text.isdigit():
+        await update.message.reply_text("فقط شماره وارد کنید")
+        return DELETE_TASK
+      
+      else:
+              index = int(update.message.text)-1
+              if 0 <= index < len(context.user_data["tasks"]):
+                     context.user_data["tasks"].pop(index)
+                     await update.message.reply_text(
+                            "کار مدنظر حذف شد"
+                     )
+                     return ConversationHandler.END
+
+              else:
+                     await update.message.reply_text(
+                            "شماره کار مد نظر وجود ندار دوباره امتحان کنید"
+                     )
+                     return DELETE_TASK
+
 
 app = ApplicationBuilder().token(TOKEN).build()
+       
+
+add_task_handler = ConversationHandler(
+    entry_points=[
+        MessageHandler(
+            filters.Regex("^➕ افزودن کار$"),
+            start_add_task
+        )
+    ],
+
+    states={
+        ADD_TASK: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                save_task
+            )
+        ]
+    },
+
+    fallbacks=[]
+)
+
+delete_task_handler = ConversationHandler(
+      entry_points=[
+            MessageHandler(
+                  filters.Regex("^❌ حذف کار مد نظر$"),
+                  get_index
+            )
+      ],
+
+      states={
+          DELETE_TASK : [
+                MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                delete_task      
+                )
+          ]  
+      },
+
+      fallbacks=[]
+
+)
+
+
+
 
 
 app.add_handler(
    CommandHandler("start", start)    
 )
 
+app.add_handler(add_task_handler)
 
 app.add_handler(
-    MessageHandler(
-        filters.TEXT & ~filters.COMMAND,
-        menu_handler
-    )
+      MessageHandler(
+            filters.Regex("^📋 نمایش کارها$"),
+            display
+      )
 )
+
+app.add_handler(
+      MessageHandler(
+            filters.Regex("^🗑 حذف همه کارها$"),
+            delight_all_task
+      )
+)
+
+app.add_handler(delete_task_handler)
+
+
+
+
 
 
 print("ربات روشن شد...")
