f8829deaccc44eb55b64d3b1a048d684b9a029e5
sajad <ssmzr0@gmail.com>
2026-06-07 14:21:02 +0430

    22

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from telegram import Update
 from telegram import KeyboardButton
 from telegram import ReplyKeyboardMarkup
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 
 from telegram.ext import (
     ContextTypes,
@@ -13,200 +15,369 @@ from telegram.ext import (
 )
 
 TOKEN = "8721747745:AAF5r6fJdxKheM4DKgNKKsg2WMMIoquNJ34"
-ADD_TASK , DELETE_TASK = range(2)
+GET_NAME, GET_AGE, GET_COURSE, GET_NEW_PROFILE, GET_NEW_NAME, GET_NEW_AGE, GET_NEW_COURSE = range(7)
 
 keyboard = [
-    [KeyboardButton("➕ افزودن کار")],
-    [KeyboardButton("📋 نمایش کارها")],
-    [KeyboardButton("🗑 حذف همه کارها"),
-     KeyboardButton("❌ حذف کار مد نظر")]
+    [KeyboardButton("ثبت‌  نام")],
+    [KeyboardButton("مشاهده اطلاعات من")],
+    [KeyboardButton("ویرایش اطلاعات"),
+     KeyboardButton("حذف اطلاعات")]
 ]
 
-markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+keyboard2 = [
+    [InlineKeyboardButton("تغیر نام", callback_data="1")],
+    [InlineKeyboardButton("تغیر سن", callback_data="2")],
+    [InlineKeyboardButton("تغیر دوره آموزشی", callback_data="3")]
+]
+
+markup = ReplyKeyboardMarkup(keyboard, resize_keyboard= True)
+markup2 = InlineKeyboardMarkup(keyboard2)
+
+
 
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        if "tasks" not in context.user_data:
-              context.user_data["tasks"] = []
-
-        await update.message.reply_text(
-              "یک گزینه انتخاب کن:",
-              reply_markup=markup
-              )
-
-
-
-
-
-async def start_add_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-       await update.message.reply_text(
-              "کار مورد نظر را وارد کن:"
-       )
-       return ADD_TASK
-
-async def save_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-       task = update.message.text
-       context.user_data["tasks"].append(task)
-       await update.message.reply_text(
-        "کار اضافه شد ✅"
-               )
-
-       return ConversationHandler.END
-
-
-
-async def display(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    if not context.user_data.get("tasks"):
-        await update.message.reply_text("هیچ کاری ثبت نشده است.")
-        return ConversationHandler.END
-
-    else:
-        task_text = "\n".join(
-            f"{index}. {task}"
-            for index, task in enumerate(
-                context.user_data["tasks"],
-                start=1
-            )
-        )
-
-        await update.message.reply_text(task_text)
-        return ConversationHandler.END
-
-
-
-async def delight_all_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-      if context.user_data["tasks"] != [] :
-            context.user_data["tasks"] = []
-            await update.message.reply_text( "همه کارها حذف شدند")
-
-      else:
-            await update.message.reply_text( "هیچ کاری برای حذف وجود ندارد")
-
-
-
-async def get_index(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    if not context.user_data["tasks"]:
-        await update.message.reply_text(
-            "هیچ کاری برای حذف وجود ندارد"
-        )
-        return ConversationHandler.END
-
-    task_text = "\n".join(
-        f"{index}. {task}"
-        for index, task in enumerate(
-            context.user_data["tasks"],
-            start=1
-        )
-    )
+    if not "info" in context.user_data:
+        context.user_data["info"] = []
 
     await update.message.reply_text(
-        f"""لیست کارها:
-
-{task_text}
-
-شماره کاری که می‌خواهی حذف شود را وارد کن:
-"""
+        "ربات ثبت نام",
+        reply_markup = markup
     )
 
-    return DELETE_TASK
 
-async def delete_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
-      if not update.message.text.isdigit():
-        await update.message.reply_text("فقط شماره وارد کنید")
-        return DELETE_TASK
-      
-      else:
-              index = int(update.message.text)-1
-              if 0 <= index < len(context.user_data["tasks"]):
-                     context.user_data["tasks"].pop(index)
-                     await update.message.reply_text(
-                            "کار مدنظر حذف شد"
-                     )
-                     return ConversationHandler.END
 
-              else:
-                     await update.message.reply_text(
-                            "شماره کار مد نظر وجود ندار دوباره امتحان کنید"
-                     )
-                     return DELETE_TASK
+
+#ثبت نام 
+async def start_registration(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["info"] = []
+    await update.message.reply_text(
+        "نام خود را وارد کنید:"
+        )
+    return GET_NAME
+
+
+
+
+async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.text.strip()
+    if name.isalpha() :
+        context.user_data["info"].append(name)
+        await update.message.reply_text(
+            """نام شما ثبت شد
+            لطفا سن خود را وارد کنید :"""
+        )
+        return GET_AGE
+    else:
+        await update.message.reply_text(
+            "ورودی قابل قبول نیست دوباره امتحان کنید"
+        )
+        return GET_NAME
+
+
+
+
+
+async def get_age(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    age = update.message.text.strip()
+    if age.isdigit() and 0 < int(age) <= 120 :
+        context.user_data["info"].append(int(age))
+        await update.message.reply_text(
+            """سن شما ثبت شد
+           لطفا شماره دوره آموزشی خود را از لیست زیر انتخاب کنید:"""
+        )
+        await update.message.reply_text(
+            """
+            1.پایتون
+            2.جانگو
+            3.ربات تلگرام
+            4.هوش مصنوعی
+            """
+        )
+
+        return GET_COURSE
+    else:
+        await update.message.reply_text(
+            "ورودی قابل قبول نیست دوباره امتحان کنید"
+        )
+        return GET_AGE
+    
+
+
+
+
+async def get_course(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    course = update.message.text.strip()
+    if course.isdigit() and 0 < int(course) < 5 :
+        match(course):
+            case("1"):
+                context.user_data["info"].append("پایتون")
+            case("2"):
+                context.user_data["info"].append("جانگو")
+            case("3"):
+                context.user_data["info"].append("ربات تلگرام")
+            case("4"):
+                context.user_data["info"].append("هوش مصنوعی")
+
+        await update.message.reply_text(
+            f"""
+                name.   {context.user_data["info"][0]}
+                age.    {context.user_data["info"][1]}
+                course. {context.user_data["info"][2]}
+
+             """
+        )
+
+        return ConversationHandler.END
+    
+    else:
+        await update.message.reply_text(
+            "ورودی قابل قبول نیست دوباره امتحان کنید"
+        )
+        return GET_COURSE
+
+
+
+
+#مشاهده اطلاعات 
+async def display(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.user_data.get("info"):
+        await update.message.reply_text(
+            "چیزی برای نمایش وجود ندارد"
+            )
+        return ConversationHandler.END
+    else:
+        await update.message.reply_text(
+                f"""
+                    profile:
+
+                    name.   {context.user_data["info"][0]}
+                    age.    {context.user_data["info"][1]}
+                    course. {context.user_data["info"][2]}
+
+                """
+            )
+        return ConversationHandler.END
+    
+
+
+#تغیر مشخصات
+async def edit_profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.user_data.get("info"):
+        await update.message.reply_text("ابتدا ثبت نام کنید")
+        return ConversationHandler.END
+    await update.message.reply_text(
+        "کدام گزینه  مد نظر را انتخاب کنید",
+        reply_markup=markup2
+    )
+    return GET_NEW_PROFILE
+
+async def get_new_profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    match (query.data):
+        case("1"):
+            await query.message.reply_text(
+                "نام جدید را وارد کنید "
+             )
+            return GET_NEW_NAME
+        
+        case("2"):
+            await query.message.reply_text(
+                "سن جدید را وارد کنید "
+             )
+            return GET_NEW_AGE
+        
+        case("3"):
+            await query.message.reply_text(
+                """
+               : دوره جدید را وارد کنید 
+
+                1.پایتون
+                2.جانگو
+                3.ربات تلگرام
+                4.هوش مصنوعی
+            """
+             )
+        case _:
+                await query.message.reply_text("گزینه نامعتبر")
+                return GET_NEW_PROFILE
+    return GET_NEW_COURSE
+        
+
+async def get_new_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.text.strip()
+    if name.isalpha() :
+        context.user_data["info"][0] = name
+        await update.message.reply_text(
+            "نام جدید ثبت شد"
+        )
+        return ConversationHandler.END
+    else:
+        await update.message.reply_text(
+            "ورودی قابل قبول نیست دوباره امتحان کنید"
+        )
+        return GET_NEW_NAME
+
+
+async def get_new_age(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    age = update.message.text.strip()
+    if age.isdigit() and 0 < int(age) <= 120 :
+        context.user_data["info"][1] = int(age)
+        await update.message.reply_text(
+            "سن جدید شما ثبت شد"
+        )
+        
+        return ConversationHandler.END
+    else:
+        await update.message.reply_text(
+            "ورودی قابل قبول نیست دوباره امتحان کنید"
+        )
+        return GET_NEW_AGE
+    
+
+async def get_new_course(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    course = update.message.text.strip()
+    if course.isdigit() and 0 < int(course) < 5:
+        match(course):
+            case("1"):
+                context.user_data["info"][2] = "پایتون"
+            case("2"):
+                context.user_data["info"][2] = "جانگو"
+            case("3"):
+                context.user_data["info"][2] = "ربات تلگرام"
+            case("4"):
+                context.user_data["info"][2] = "هوش مصنوعی"
+            
+
+        await update.message.reply_text(
+           "دوره جدید ثبت شدش"
+        )
+
+        return ConversationHandler.END
+    
+    else:
+        await update.message.reply_text(
+            "ورودی قابل قبول نیست دوباره امتحان کنید"
+        )
+        return GET_NEW_COURSE
+    
+
+
+#بخش حذف همه اطلاعات
+async def delete_profil(update: Update, context: ContextTypes.DEFAULT_TYPE):
+     if not context.user_data.get("info"):
+        await update.message.reply_text(
+            "چیزی برای حذف کردن وجود ندار"
+            )
+        return ConversationHandler.END
+     else:
+         context.user_data["info"] = []
+         await update.message.reply_text(
+            "همه اطلاعات حذف شد"
+            )
+         return ConversationHandler.END
+
+async def cancel (update: Update, context: ContextTypes.DEFAULT_TYPE):
+       await update.message.reply_text("❌ ثبت نام لغو شد.")
+       return ConversationHandler.END   
+
 
 
 app = ApplicationBuilder().token(TOKEN).build()
-       
 
-add_task_handler = ConversationHandler(
+set_profile = ConversationHandler(
     entry_points=[
         MessageHandler(
-            filters.Regex("^➕ افزودن کار$"),
-            start_add_task
+            filters.Regex("^ثبت  نام$"),
+            start_registration
         )
     ],
-
     states={
-        ADD_TASK: [
+        GET_NAME: [
+             MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                get_name     
+                )
+        ],
+        GET_AGE: [
             MessageHandler(
                 filters.TEXT & ~filters.COMMAND,
-                save_task
-            )
+                get_age     
+                )
+        ],
+        GET_COURSE: [
+             MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                get_course     
+                )
         ]
     },
-
-    fallbacks=[]
+    fallbacks=[
+            CommandHandler("cancel", cancel)
+    ]
 )
 
-delete_task_handler = ConversationHandler(
-      entry_points=[
+change_profile = ConversationHandler(
+    entry_points=[
             MessageHandler(
-                  filters.Regex("^❌ حذف کار مد نظر$"),
-                  get_index
+                 filters.Regex("^ویرایش اطلاعات$"),
+                 edit_profile
             )
-      ],
-
-      states={
-          DELETE_TASK : [
-                MessageHandler(
+    ],
+    states={
+        
+        GET_NEW_PROFILE: [
+            CallbackQueryHandler(get_new_profile)
+        ],
+        GET_NEW_NAME: [
+            MessageHandler(
                 filters.TEXT & ~filters.COMMAND,
-                delete_task      
+                get_new_name     
                 )
-          ]  
-      },
-
-      fallbacks=[]
-
+        ],
+        GET_NEW_AGE: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                get_new_age     
+                )
+        ],
+        GET_NEW_COURSE: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                get_new_course   
+                )
+        ]
+    },
+    fallbacks=[
+            CommandHandler("cancel", cancel)    
+    ]
 )
 
+app.add_handler(
+   CommandHandler("start", start)     
+)
 
+app.add_handler(set_profile)
+
+app.add_handler(change_profile)
+
+app.add_handler(
+    MessageHandler(
+        filters.Regex("^مشاهده اطلاعات من$"),
+        display
+    )
+)
 
 
 
 app.add_handler(
-   CommandHandler("start", start)    
+    MessageHandler(
+        filters.Regex("^حذف اطلاعات$"),
+        delete_profil
+    )
 )
-
-app.add_handler(add_task_handler)
-
-app.add_handler(
-      MessageHandler(
-            filters.Regex("^📋 نمایش کارها$"),
-            display
-      )
-)
-
-app.add_handler(
-      MessageHandler(
-            filters.Regex("^🗑 حذف همه کارها$"),
-            delight_all_task
-      )
-)
-
-app.add_handler(delete_task_handler)
-
-
-
-
 
 
 print("ربات روشن شد...")
